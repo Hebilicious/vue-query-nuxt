449c6b208dc53067990d00db2bbd0c222ee03dc1
Hebilicious <xsh4k3@gmail.com>
2023-08-05 20:06:35 +0700

    docs: add wagmi example

---
+++ playgrounds/wagmi/app.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const { data, isLoading } = useQuery({
+  queryKey: ["todos"],
+  queryFn: () => $fetch("/api/todos")
+})
+const { $wagmi } = useNuxtApp()
+</script>
+
+<template>
+  <div>
+    <h1>Todos</h1>
+    <p v-if="isLoading">
+      Loading
+    </p>
+    <pre>{{ data }}</pre>
+  </div>
+</template>
---
+++ playgrounds/wagmi/nuxt.config.ts
@@ -0,0 +1,7 @@
+export default defineNuxtConfig({
+  modules: [
+    "../../packages/vue-query-nuxt/src/module.ts",
+    "@use-wagmi/nuxt"
+    // "@hebilicious/vue-query-nuxt"
+  ]
+})
