b453fcd75883e9a19d0f6c1b6fd8f84104072709
n0099 <n@n0099.net>
2025-05-27 02:24:46 +0000

    feat: (de)serialize any instances of `Error` in `QueryState.error` while (de)hydrating

--- packages/vue-query-nuxt/src/runtime/plugin.ts
+++ packages/vue-query-nuxt/src/runtime/plugin.ts
@@ -1,5 +1,6 @@
 import type { DehydratedState } from "@tanstack/vue-query"
 import { QueryClient, VueQueryPlugin, dehydrate, hydrate } from "@tanstack/vue-query"
+import { isErrorLike, deserializeError, serializeError } from 'serialize-error';
 import { getVueQueryOptions } from "./utils"
 import { pluginHook } from "#build/internal.vue-query-plugin-hook"
 import { defineNuxtPlugin, useRuntimeConfig, useState } from "#imports"
@@ -14,13 +15,35 @@ export default defineNuxtPlugin((nuxt) => {
 
   nuxt.vueApp.use(VueQueryPlugin, { queryClient, ...vueQueryPluginOptions, ...hookOptions })
 
+  type Dehydrated = ReturnType<typeof dehydrate>
+  type QueryOrMutations = Dehydrated['queries'] | Dehydrated['mutations']
   if (import.meta.server) {
     nuxt.hooks.hook("app:rendered", () => {
-      vueQueryState.value = dehydrate(queryClient, dehydrateOptions)
+      const serializeErrors = <T extends QueryOrMutations>(queryOrMutations: T): T => queryOrMutations
+        .filter(queryOrMutation => queryOrMutation.state.error instanceof Error)
+        .map(queryOrMutation => {
+          queryOrMutation.state.error = serializeError(queryOrMutation.state.error) as Error | null
+          return queryOrMutation
+        }) as T
+      const dehydrated = dehydrate(queryClient, dehydrateOptions)
+      dehydrated.queries = serializeErrors(dehydrated.queries)
+      dehydrated.mutations = serializeErrors(dehydrated.mutations)
+      vueQueryState.value = dehydrated
     })
   }
 
-  if (import.meta.client) hydrate(queryClient, vueQueryState.value, hydrateOptions)
+  if (import.meta.client) {
+    const deserializeErrors = <T extends QueryOrMutations>(queryOrMutations: T): T => queryOrMutations
+        .filter(queryOrMutation => isErrorLike(queryOrMutation.state.error))
+        .map(queryOrMutation => {
+          queryOrMutation.state.error = deserializeError(queryOrMutation.state.error)
+          return queryOrMutation
+        }) as T
+    const dehydrated = vueQueryState.value
+    dehydrated.queries = deserializeErrors(dehydrated.queries)
+    dehydrated.mutations = deserializeErrors(dehydrated.mutations)
+    hydrate(queryClient, dehydrated, hydrateOptions)
+  }
 
   if (pluginReturn !== undefined) return pluginReturn
   return;
